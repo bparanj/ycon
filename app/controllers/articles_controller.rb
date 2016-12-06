b3bde00f7a6a0e72896edbee26d72a1775475ef7
Bala Paranj <bparanj@onekingslane.com>
2016-12-06 12:44:07 -0800

    blog starting point

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "secret",
+                               except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(allowed_params)
+    if @article.save
+      redirect_to @article, notice: "Created article."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(allowed_params)
+      redirect_to @article, notice: "Updated article."
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def allowed_params
+    params.require(:article).permit(:name, :published_on, :content)
+  end
+end
